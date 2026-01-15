0388a6550e247c2b5ebc403b4ed5548cb46c008e
pankajgour12 <pankajgour5000@gmail.com>
2026-01-15 20:59:23 +0530

    Create Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Agence from './pages/Agence'
+import Project from './pages/Project'
 
 const App = () => {
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/agence' element={<Agence/>}/>
+        <Route path='/project' element={<Project/>}/>
+
+
+
+      </Routes>
+
+
     </div>
   )
 }
