756f1d253bb1ceeab05df4e5e1ace80a93ae33fa
pankajgour12 <pankajgour5000@gmail.com>
2026-01-16 18:25:51 +0530

    Images Animation

--- src/components/home/HomeBottom.jsx
+++ src/components/home/HomeBottom.jsx
@@ -3,9 +3,20 @@ import { Link } from 'react-router-dom'
 
 const HomeBottom = () => {
   return (
-    <div className='flex flex-col pt-6 ' >
-      <div className='self-end mr-6'>
+  
+
+
+     <div className='flex flex-col pt-6 ' >
+      <div className='self-end mr-8'>
         <p className='text-start  text-[1vw] font-[font2] '>
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
        
          K72 est une agence qui pense
          <br /> chaque action pour nourrir la marque.
@@ -20,13 +31,15 @@ const HomeBottom = () => {
 
 
       <div className='flex font-[font1]  items-center justify-center gap-2 mt-6'>
-        <Link className='text-[6.5vw] leading-[5.5vw] mt-6 border-white rounded-full px-14 py-2  uppercase border-4 '  to='/project'>Project</Link>
-        <Link className='text-[6.5vw] leading-[5.5vw] mt-6 border-white rounded-full px-14 py-2  uppercase border-4 '  to='/agence'>Agence</Link>
+        <Link className='text-[6.5vw] leading-[5.5vw] mt-6 border-white hover:text-[#D3FD50] hover:border-[#D3FD50]  rounded-full px-14 py-2  uppercase border-4 '  to='/project'>Project</Link>
+        <Link className='text-[6.5vw] leading-[5.5vw] mt-6 border-white hover:text-[#D3FD50] hover:border-[#D3FD50]  rounded-full px-14 py-2  uppercase border-4 '  to='/agence'>Agence</Link>
        
       </div>
       
       
     </div>
+
+  
   )
 }
 
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,11 +1,89 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import React, { useRef } from "react";
+import img1 from '../assets/1.jpg'
+import img2 from '../assets/2.jpg'
+import img3 from '../assets/3.jpg'
+import img4 from '../assets/4.jpg'
 
 const Agence = () => {
-  return (
-    <div>
-      <h1> Agence</h1>
-    </div>
-  )
-}
 
-export default Agence
+   const imageDivRef = useRef(null);
+   gsap.registerPlugin(ScrollTrigger);
+
+   useGSAP(function(){
+
+    gsap.to(imageDivRef.current,{
+     
+    scrollTrigger:{
+        trigger:imageDivRef.current,
+        markers:true,
+        start:'top 22%',
+        end:'top -100%',
+        scrub:true,
+        pin:true
+      }
+      
+      
+      
+    })
+   })
+
+
+
+
+
+  return (
+ <div>
+    <div className="section1">
+      <div ref={imageDivRef} className="absolute overflow-hidden h-[20vw] rounded-4xl w-[15vw] top-40 left-[30vw] ">
+        <img className="h-full w-full object-cover" 
+         src={img1}
+alt="" />
+      </div>
+
+
+
+      <div className="relative font-[font2] ">
+      <div className="mt-[55vh]">
+        <h1 className="text-[19vw] text-center uppercase leading-[17vw] ">
+          Soixan7e <br />
+          Douze
+        </h1>
+        
+      </div>
+
+      <div className="pl-[45%] font-[font1] mt-10">
+        <p className="text-5xl "> 
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          
+         
+          
+          Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.
+        </p>
+      </div>
+      </div>
+    </div>
+
+    <div className="section2 h-screen">
+      
+    </div>
+
+ </div>
+   
+    
+  );
+};
+
+export default Agence;
+//  #D3FD50
