609dc3e15cbe9bff72b65a109bd7383a0a74b778
pankajgour12 <pankajgour5000@gmail.com>
2026-01-19 22:29:09 +0530

    homehero Responsive

--- src/components/home/HomeHero.jsx
+++ src/components/home/HomeHero.jsx
@@ -4,15 +4,15 @@ import Video from './Video'
 
 const HomeHero = () => {
   return (
-    <div className='font-[font1] pt-5 text-center '>
-      <div className='text-[9.4vw] uppercase leading-[8vw] flex items-center justify-center '>L'étincelle</div>
-      <div className='text-[9.4vw] uppercase leading-[8vw] flex items-center justify-center '>qui 
+    <div className='font-[font1] mt-90 lg:mt-0 text-center '>
+      <div className='lg:text-[9.4vw] text-[13vw] uppercase lg:leading-[8vw] leading-[11vw] flex items-center justify-center '>L'étincelle</div>
+      <div className='lg:text-[9.4vw] text-[13vw] uppercase lg:leading-[8vw] leading-[11vw] flex items-center justify-center '>qui 
         <div 
         className='h-[7vw] w-[15vw] -mt-3 rounded-full overflow-hidden'  > <Video/> 
          </div> génère 
       
       </div>
-      <div className='text-[9.4vw] uppercase leading-[8vw] flex items-center justify-center '>la créativité</div>
+      <div className='lg:text-[9.4vw] text-[13vw] uppercase lg:leading-[8vw]  leading-[11vw] flex items-center justify-center '>la créativité</div>
 
 
 
