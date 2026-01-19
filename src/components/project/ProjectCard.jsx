0cd8598970ae454a9ce641b362081327a6baefca
pankajgour12 <pankajgour5000@gmail.com>
2026-01-19 21:55:55 +0530

    fix all aniamations bugs

--- src/components/project/ProjectCard.jsx
+++ src/components/project/ProjectCard.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
 
-
 const ProjectCard = (props) => {
   return (
-    <>  
-              <div className='w-1/2 group  transition-all relative rounded-none hover:rounded-4xl overflow-hidden h-full'>
-                <img className='h-full w-full object-cover transition-transform duration-300 ease-out group-hover:scale-110' src={props.image1} alt="" />
-                <div className='opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center top-0 left-0 h-full w-full bg-black/10 '>
-                   <h2 className='uppercase text-6xl font-[font2] border-2 pt-1 px-10 text-white border-white rounded-full '>voir le projet</h2>
-                </div>
-                
-              </div>
-                <div className='w-1/2 group  transition-all relative rounded-none hover:rounded-4xl overflow-hidden mb-3 h-full'>
-                <img className='h-full w-full object-cover transition-transform duration-300 ease-out group-hover:scale-110' src={props.image2} alt="" />
-                <div className='opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center top-0 left-0 h-full w-full bg-black/10 '>
-                   <h2 className='uppercase text-6xl font-[font2] border-2 pt-1 px-10 text-white border-white rounded-full '>voir le projet</h2>
-                </div>
-                
-              </div>
-    
-          </>
+  <>  
+        <div className='w-1/2 group relative transition-all rounded-none hover:rounded-[70px] overflow-hidden h-full '>
+            <img src={props.image1} alt="nav1" className='w-full h-full object-cover'/>
+             <div className='opacity-0 group-hover:opacity-100 transition-all absolute top-0 left-0 flex items-center justify-center h-full w-full bg-black/15'>
+              <h1 className='uppercase text-6xl font-[font2] border-2 pt-2 px-8 text-white border-white rounded-full' > 
+                Voir le projet
+              </h1>
+             </div>
+            
+            </div>
+
+            <div className='w-1/2 group relative transition-all rounded-none hover:rounded-[70px] overflow-hidden h-full '>
+            <img src={props.image2} alt="nav1" className='w-full h-full object-cover'/>
+             <div className='opacity-0 group-hover:opacity-100 transition-all absolute top-0 left-0 flex items-center justify-center h-full w-full bg-black/15'>
+              <h1 className='uppercase text-6xl font-[font2] border-2 pt-2 px-8 text-white border-white rounded-full' > 
+                Voir le projet
+              </h1>
+             </div>
+            
+            </div>
+            
+
+         
+    </>
   )
 }
 
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -8,15 +8,11 @@ import ProjectCard from '../components/project/ProjectCard.jsx'
  import p6 from '../assets/p6.jpg'
  import p7 from '../assets/p7.jpg'
  import p8 from '../assets/nav4.png'
- import p9 from '../assets/p9.jpg'
- import p10 from '../assets/p10.jpg'
- import p11 from '../assets/p11.jpg'
- import p12 from '../assets/p12.jpg'
- import p13 from '../assets/p13.jpg'
- import p14 from '../assets/p14.jpg'
- import p15 from '../assets/p15.jpg'
+ 
+ 
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
  
 
 
@@ -41,52 +37,32 @@ const projects = [
     image1: p7,
     image2: p8,
   },
-  {
-    image1: p9,
-    image2: p10,
-  },
-  {
-    image1: p11,
-    image2: p12,
-  },
-  {
-    image1: p13,
-    image2: p14,
-  },
-  {
-    image1: p15,
-    image2: p5,
-  },
+ 
 
 ]
 
 
 
-
-
-
- useGSAP(() => {
-  gsap.utils.toArray('.hero').forEach((card) => {
-    gsap.from(card, {
-      height: '100px',
+  gsap.registerPlugin(ScrollTrigger);
+  
+  useGSAP(function(){
+    gsap.from('.hero',{
+      height:'100px',
       stagger:{
-      amount:0.4
-     },          
-      scrollTrigger: {
-        trigger: card,         
-        start: 'top 100%',      
-        end: 'top -150%',         
-        scrub: 1,
-        pin: true,
-        markers: true,
+        amount:0.4
+
       },
+      scrollTrigger:{
+        trigger:'.lol',
+        
+        start:'top 100%',
+        end:'top -200%',
+        scrub:true
+      }
+       
     })
+
   })
-}) 
-
-
-
-
 
 
 
@@ -111,29 +87,28 @@ const projects = [
 
 
   return (
-    <div className='p-4 text-black'>
-      <div className='mt-[40vh]' >
-        <h2 className='font-[font2] text-[13vw]  uppercase '> projets</h2>
+    <div className='p-4 mb-[100vh] text-black'>
+      <div className='pt-[40vh]' >
+        <h2 className='font-[font2] text-[13vw] uppercase '> projets 
+       </h2>
       </div>
 
-      
+      <div className='-mt-20 lol'>
 
+        {
+          projects.map(function(elem , idx){
+         return  <div key={idx} className='hero w-full  h-[520px] mb-4 flex gap-4 '> 
+             
+             <ProjectCard  image1={elem.image1} image2={elem.image2}/>
 
+             </div>
+          })}
+        
 
-      
-      <div className='-mt-20 lol '>
-        {projects.map(function(elem,index){
-        return <div key={index} className='hero overflow-hidden w-full mb-4 flex gap-4' >
-        <ProjectCard image1={elem.image1} image2={elem.image2}/>
-        </div>
-       })}
-  
-</div>
-
-     
-
+      </div>
     </div>
   )
+
 }
 
 export default Project
