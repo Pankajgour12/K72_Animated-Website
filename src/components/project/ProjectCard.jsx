e95319cddb0ef2455094180324fecdd62d874d25
pankajgour12 <pankajgour5000@gmail.com>
2026-01-19 09:37:12 +0530

    img update

---
+++ src/components/project/ProjectCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+const ProjectCard = (props) => {
+  return (
+    <div className='w-full h-150 mb-4 flex gap-4  ' >
+              <div className='w-1/2 group  transition-all relative rounded-none hover:rounded-4xl overflow-hidden h-full'>
+                <img className='h-full w-full object-cover transition-transform duration-300 ease-out group-hover:scale-110' src={props.image1} alt="" />
+                <div className='opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center top-0 left-0 h-full w-full bg-black/10 '>
+                   <h2 className='uppercase text-6xl font-[font2] border-2 pt-1 px-10 text-white border-white rounded-full '>voir le projet</h2>
+                </div>
+                
+              </div>
+                <div className='w-1/2 group  transition-all relative rounded-none hover:rounded-4xl overflow-hidden h-full'>
+                <img className='h-full w-full object-cover transition-transform duration-300 ease-out group-hover:scale-110' src={props.image2} alt="" />
+                <div className='opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center top-0 left-0 h-full w-full bg-black/10 '>
+                   <h2 className='uppercase text-6xl font-[font2] border-2 pt-1 px-10 text-white border-white rounded-full '>voir le projet</h2>
+                </div>
+                
+              </div>
+    
+            </div>
+  )
+}
+
+export default ProjectCard
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import ProjectCard from '../components/project/ProjectCard.jsx'
  import nav1 from '../assets/nav1.png'
  import nav2 from '../assets/nav1.jpg'
+ import p3 from '../assets/p3.jpg'
+ import p4 from '../assets/p4.jpg'
+ import p5 from '../assets/p5.jpg'
+ import p6 from '../assets/p6.jpg'
+ import p7 from '../assets/p7.jpg'
+ import p8 from '../assets/nav4.png'
+ import p9 from '../assets/p9.jpg'
+ import p10 from '../assets/p10.jpg'
+ import p11 from '../assets/p11.jpg'
+ import p12 from '../assets/p12.jpg'
+ import p13 from '../assets/p13.jpg'
+ import p14 from '../assets/p14.jpg'
+ import p15 from '../assets/p15.jpg'
+ 
+
 
 
 const Project = () => {
+const projects = [
+  {
+    image1: nav1,
+    image2: nav2,
+  },
+  {
+    image1: p3,
+    image2: p4,
+  },
+  {
+    image1: p5,
+    image2: p6,
+  },
+  {
+    image1: p7,
+    image2: p8,
+  },
+  {
+    image1: p9,
+    image2: p10,
+  },
+  {
+    image1: p11,
+    image2: p12,
+  },
+  {
+    image1: p13,
+    image2: p14,
+  },
+  {
+    image1: p15,
+    image2: p5,
+  },
+
+]
+
   return (
     <div className='p-4 text-black'>
       <div className='pt-[42vh]' >
@@ -11,23 +63,9 @@ const Project = () => {
       </div>
 
       <div className='-mt-20'>
-        <div className='w-full h-150 mb-4 flex gap-4  ' >
-          <div className='w-1/2 group  transition-all relative rounded-none hover:rounded-4xl overflow-hidden h-full'>
-            <img className='h-full w-full object-cover transition-transform duration-300 ease-out group-hover:scale-110' src={nav1} alt="" />
-            <div className='opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center top-0 left-0 h-full w-full bg-black/10 '>
-               <h2 className='uppercase text-6xl font-[font2] border-2 pt-1 px-10 text-white border-white rounded-full '>voir le projet</h2>
-            </div>
-            
-          </div>
-            <div className='w-1/2 group  transition-all relative rounded-none hover:rounded-4xl overflow-hidden h-full'>
-            <img className='h-full w-full object-cover transition-transform duration-300 ease-out group-hover:scale-110' src={nav2} alt="" />
-            <div className='opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center top-0 left-0 h-full w-full bg-black/10 '>
-               <h2 className='uppercase text-6xl font-[font2] border-2 pt-1 px-10 text-white border-white rounded-full '>voir le projet</h2>
-            </div>
-            
-          </div>
-
-        </div>
+       {projects.map(function(elem){
+        return <ProjectCard image1={elem.image1} image2={elem.image2}/>
+       })}
         
       </div>
      
